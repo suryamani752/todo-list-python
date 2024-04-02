2fa1ab53068427d588ec36a537d9a231fecb5b4c
Suryamani Kumar <suryamani.kumar.external@idiada.com>
2024-04-02 10:56:03 +0530

    updated

--- todolist/settings.py
+++ todolist/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/app/db.sqlite3',
     }
 }
 
